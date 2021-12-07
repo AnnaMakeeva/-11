51405979095d3921b2d57c6a2feae956ebc890d3
Anna <anna.vm@mail.ru>
2021-12-07 20:48:32 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задание_11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LineParam MyEquation = new LineParam();  
+
+            MyEquation.k = 10;
+            MyEquation.b = 20;
+
+            MyEquation.ResolveEquation();
+
+            Console.WriteLine("Ответ: {0}", MyEquation.result);
+
+            Console.ReadKey();
+
+        }
+    }
+
+    public struct LineParam
+    {
+        public double k;
+        public double b;
+        public double result;
+
+        public void ResolveEquation()
+        {
+            result = -b / k;
+        }
+    }
+
+}
